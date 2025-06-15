0bdb9bec0a7df6d513b6b8ec3abdfb8eebe36b9e
shorryah <shaurya.sh05@gmail.com>
2025-06-15 22:09:01 +0800

    Updated models.py based on database requirements

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
 
-class Patient(BaseModel):
+class PatientCreate(BaseModel):
     name: str
     age: int
     condition: str
+
+class Patient(PatientCreate):
+    id: str 
     created_at: int = Field(default_factory=lambda: int(datetime.timestamp(datetime.now())))
-    updated_at: int = Field(default_factory=lambda: int(datetime.timestamp(datetime.now())))
+
